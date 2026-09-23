1be39038f6c86fed284565aa92f0c5adfbee726e
mrSogib <83939055+mrsogib@users.noreply.github.com>
2026-09-23 22:35:39 +0600

    Update Type.kt

--- app/src/main/java/com/aurawave/launcher/ui/theme/Type.kt
+++ app/src/main/java/com/aurawave/launcher/ui/theme/Type.kt
@@ -1,3 +1,26 @@
+package com.aurawave.launcher.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
+
+
+
+
+
+
 // package com.aurawave.launcher.ui.theme
 
 // import androidx.compose.material3.Typography
@@ -16,20 +39,3 @@
 
 
 
-package com.aurawave.launcher.ui.theme
-
-import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.SansSerif,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-)
