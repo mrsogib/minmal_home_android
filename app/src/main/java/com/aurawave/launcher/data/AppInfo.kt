f4527d39400c9ec66afcf7df09c519184fa235c9
mrSogib <83939055+mrsogib@users.noreply.github.com>
2026-09-23 20:23:28 +0600

    Update AppInfo.kt

--- app/src/main/java/com/aurawave/launcher/data/AppInfo.kt
+++ app/src/main/java/com/aurawave/launcher/data/AppInfo.kt
@@ -1,13 +1,22 @@
-package com.aurawave.launcher.data
+// package com.aurawave.launcher.data
 
-import android.graphics.drawable.Drawable
+// import android.graphics.drawable.Drawable
+
+// data class AppInfo(
+//     val label: String,
+//     val packageName: String,
+//     val activityName: String,
+//     val icon: Drawable,
+//     val isHidden: Boolean = false
+// ) {
+//     val componentKey: String get() = "$packageName/$activityName"
+// }
+
+
+
+package com.aurawave.launcher.data
 
 data class AppInfo(
     val label: String,
-    val packageName: String,
-    val activityName: String,
-    val icon: Drawable,
-    val isHidden: Boolean = false
-) {
-    val componentKey: String get() = "$packageName/$activityName"
-}
+    val packageName: String
+)
