17331ff6bcd40b0d966e437514fe58923fe30931
mrSogib <83939055+mrsogib@users.noreply.github.com>
2026-09-23 20:35:26 +0600

    Update Type.kt

--- app/src/main/java/com/aurawave/launcher/ui/theme/Type.kt
+++ app/src/main/java/com/aurawave/launcher/ui/theme/Type.kt
@@ -1,14 +1,35 @@
+// package com.aurawave.launcher.ui.theme
+
+// import androidx.compose.material3.Typography
+// import androidx.compose.ui.text.TextStyle
+// import androidx.compose.ui.text.font.FontWeight
+// import androidx.compose.ui.unit.sp
+
+// val Typography = Typography(
+//     headlineLarge = TextStyle(fontWeight = FontWeight.Light, fontSize = 34.sp),
+//     titleLarge = TextStyle(fontWeight = FontWeight.Medium, fontSize = 22.sp),
+//     bodyLarge = TextStyle(fontWeight = FontWeight.Normal, fontSize = 17.sp),
+//     bodyMedium = TextStyle(fontWeight = FontWeight.Normal, fontSize = 15.sp),
+//     labelSmall = TextStyle(fontWeight = FontWeight.Medium, fontSize = 11.sp)
+// )
+
+
+
+
 package com.aurawave.launcher.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
-    headlineLarge = TextStyle(fontWeight = FontWeight.Light, fontSize = 34.sp),
-    titleLarge = TextStyle(fontWeight = FontWeight.Medium, fontSize = 22.sp),
-    bodyLarge = TextStyle(fontWeight = FontWeight.Normal, fontSize = 17.sp),
-    bodyMedium = TextStyle(fontWeight = FontWeight.Normal, fontSize = 15.sp),
-    labelSmall = TextStyle(fontWeight = FontWeight.Medium, fontSize = 11.sp)
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
 )
