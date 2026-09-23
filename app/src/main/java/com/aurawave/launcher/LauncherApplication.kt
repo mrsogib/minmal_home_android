1ffefef64d0b659acaec6730f8a36ce3aee31701
mrSogib <83939055+mrsogib@users.noreply.github.com>
2026-09-23 20:18:58 +0600

    Update LauncherApplication.kt

--- app/src/main/java/com/aurawave/launcher/LauncherApplication.kt
+++ app/src/main/java/com/aurawave/launcher/LauncherApplication.kt
@@ -1,14 +1,27 @@
 package com.aurawave.launcher
 
 import android.app.Application
-import com.aurawave.launcher.data.IconPackManager
 
 class LauncherApplication : Application() {
-    lateinit var iconPackManager: IconPackManager
-        private set
-
     override fun onCreate() {
         super.onCreate()
-        iconPackManager = IconPackManager(this)
     }
 }
+
+
+
+
+// package com.aurawave.launcher
+
+// import android.app.Application
+// import com.aurawave.launcher.data.IconPackManager
+
+// class LauncherApplication : Application() {
+//     lateinit var iconPackManager: IconPackManager
+//         private set
+
+//     override fun onCreate() {
+//         super.onCreate()
+//         iconPackManager = IconPackManager(this)
+//     }
+// }
