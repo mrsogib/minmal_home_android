2f16204bfe24a26d90586d27e1667f15f0a69100
mrSogib <83939055+mrsogib@users.noreply.github.com>
2026-09-23 20:56:01 +0600

    Update AdminReceiver.kt

--- app/src/main/java/com/aurawave/launcher/gestures/AdminReceiver.kt
+++ app/src/main/java/com/aurawave/launcher/gestures/AdminReceiver.kt
@@ -2,4 +2,5 @@ package com.aurawave.launcher.gestures
 
 import android.app.admin.DeviceAdminReceiver
 
+// System BroadcastReceiver required by Android DevicePolicyManager to allow screen locking
 class AdminReceiver : DeviceAdminReceiver()
