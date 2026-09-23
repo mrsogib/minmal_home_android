5ba9be8d7f2c53dfc9b5bcc685607e31e0ac87b1
mrSogib <83939055+mrsogib@users.noreply.github.com>
2026-09-24 00:47:16 +0600

    Delete app directory

--- app/src/main/java/com/aurawave/launcher/gestures/AdminReceiver.kt
+++
@@ -1,6 +0,0 @@
-package com.aurawave.launcher.gestures
-
-import android.app.admin.DeviceAdminReceiver
-
-// System BroadcastReceiver required by Android DevicePolicyManager to allow screen locking
-class AdminReceiver : DeviceAdminReceiver()
